387d0138c0fcb85db6e1070ad6b6525e7166ae79
Oliver Kurmis <oliver@kurmis.com>
2016-04-03 17:56:02 +0200

    test edge cases

--- test/BasicTest.php
+++ test/BasicTest.php
@@ -15,6 +15,11 @@ class BasicTest extends PHPUnit_Framework_TestCase
         
         // Assert False with error message
         $this->assertFalse($a->isPrime(13873*14107), '195706411 is not a prime number!');
+        
+        // Test edge cases
+        $this->assertTrue($a->isPrime(2), '2 is a prime number!');
+        $this->assertFalse($a->isPrime(1), '1 is not a prime number!');
+        $this->assertFalse($a->isPrime(0), '0 is not a prime number!');
 
     }
 
